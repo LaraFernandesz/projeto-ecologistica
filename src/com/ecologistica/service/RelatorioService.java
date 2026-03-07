4433e58925c556b3ccc30771ea35126f6d3289d5
LaraFernandesz <lara.marina@aluno.uepb.edu.br>
2026-03-07 15:51:02 -0300

    feat(service): adiciona estrutura inicial do RelatorioService

---
+++ src/com/ecologistica/service/RelatorioService.java
@@ -0,0 +1,13 @@
+package com.ecologistica.service;
+
+public interface RelatorioService {
+
+    void totalEntregasPorStatus();
+
+    void entregasPendentesOrdenadasPorDistancia();
+
+    void top3EntregasMaisDistantes();
+
+    void mediaDistanciaEntregasPendentes();
+
+}
