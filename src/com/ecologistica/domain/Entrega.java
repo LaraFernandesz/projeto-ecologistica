aa4847a0573f26b04289c36c6ce06c3fd54e96ff
LaraFernandesz <lara.marina@aluno.uepb.edu.br>
2026-03-07 14:35:36 -0300

    refactor(domain): Entrega passa a usar StatusEntrega enum

--- src/com/ecologistica/domain/Entrega.java
+++ src/com/ecologistica/domain/Entrega.java
@@ -1,21 +1,49 @@
 package com.ecologistica.domain;
 
+import com.ecologistica.domain.enums.StatusEntrega;
+
 public class Entrega {
+
     private Long id;
+    private String clienteOuEndereco; // opcional
     private Ponto ponto;
-    private String status;
+    private StatusEntrega status;
 
     public Entrega() {}
 
-    public Entrega(Long id, Ponto ponto, String status) {
+    public Entrega(Long id, String clienteOuEndereco, Ponto ponto, StatusEntrega status) {
         this.id = id;
+        this.clienteOuEndereco = clienteOuEndereco;
         this.ponto = ponto;
         this.status = status;
     }
 
-    public Long getId() { return id; }
-    public Ponto getPonto() { return ponto; }
-    public String getStatus() { return status; }
+    public Long getId() {
+        return id;
+    }
 
-    public void setStatus(String status) { this.status = status; }
+    public String getClienteOuEndereco() {
+        return clienteOuEndereco;
+    }
+
+    public Ponto getPonto() {
+        return ponto;
+    }
+
+    public StatusEntrega getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEntrega status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Entrega{id=" + id +
+                ", clienteOuEndereco='" + clienteOuEndereco + '\'' +
+                ", ponto=" + ponto +
+                ", status=" + status +
+                '}';
+    }
 }
